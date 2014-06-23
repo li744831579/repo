11a3e33319ba6e7d30cd050089688151f84c86ec
Jun <jun0213@163.com>
2014-06-23 20:32:18 +0800

    20140623

---
+++ finstr.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+char *findstr(char*str1,char*str2)//查看str1中是否包含str2,如果包含返回第一次出现的指针，如果不包含，返回NULL
+{
+	const char* curr=str2;
+	while(*str1 != '\0')
+	{
+		if ((*curr!='\0')&&(*str1 == *curr))
+		{
+			str1++;
+			curr++;
+			continue;
+		}else if (*curr == '\0')
+		{
+			return str1-strlen(str2);
+		}else 
+		{
+			curr = str2;
+		}
+		str1++;
+
+	}	
+	return NULL;
+
+}
+int main()
+{
+	char *s = findstr("hello world","worl"); 
+	cout << *s <<endl;
+	return 0;
+}
---
+++ s.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+
+
+	char *str="hello world";
+	cout <<strlen(str);
+		return 0;
+}
